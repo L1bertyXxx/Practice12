42a0602c019312ccb069d08e5e21eb6602bdac3b
L1berty <larsanik92@gmail.com>
2025-05-25 01:13:22 +0300

    Task_1_1

---
+++ Task_1.py
@@ -0,0 +1,13 @@
+import json
+
+with open("products.json", encoding="utf-8") as file:
+    data = json.load(file)
+
+for product in data["products"]:
+    print(f"Название: {product['name']}")
+    print(f"Цена: {product['price']}")
+    print(f"Вес: {product['weight']}")
+    if product["available"]:
+        print("Есть в наличии\n")
+    else:
+        print("Нет в наличии\n")
